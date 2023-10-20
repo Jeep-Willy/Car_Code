3de28bd807cfd9dd310a6542f2ee281789d0244f
wabowen89 <wabowen89@gmail.com>
2023-10-20 16:01:48 -0700

    split code up into different tabs for different functions

---
+++ a_setup.ino
@@ -0,0 +1,24 @@
+
+void setup()
+{
+  // Setup Serial Monitor
+  Serial.begin(9600);
+  
+  // Set all the motor control pins to outputs
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  digitalWrite(in1, LOW);
+	digitalWrite(in2, LOW);
+  
+  // Initialize RadioManager with defaults - 2.402 GHz (channel 2), 2Mbps, 0dBm
+  if (!RadioManager.init())
+    Serial.println("init failed");
+} 
+ 
+void loop()
+{
+
+motorControl();
+
+}

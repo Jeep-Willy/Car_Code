5d8da9cdc2b0d1a20227612bc79a1bfbb07f3b20
wabowen89 <wabowen89@gmail.com>
2023-10-24 10:57:05 -0700

    created b_receiveData() tab. moved all related code from motorControl(). moved call for motorControl() into b_receiveData()

---
+++ b_receiveData.ino
@@ -0,0 +1,31 @@
+void receiveData() {
+
+  if (RadioManager.available()) {
+    
+    // Wait for a message addressed to us from the client
+    uint8_t len = sizeof(buf);
+    uint8_t from;
+
+    if (RadioManager.recvfromAck(buf, &len, &from)) {
+ 
+      // Serial Print the values of joystick
+      Serial.print("Rear Motor: ");
+      Serial.print(buf[0]);
+      Serial.print(" Dir: ");
+      Serial.println(buf[1]);
+      Serial.print("Servo Motor: ");
+      Serial.print(buf[2]);
+      Serial.print(" Dir: ");
+      Serial.println(buf[3]);
+
+      motorControl();
+
+      // Send a reply back to the originator client, check for error
+      if (!RadioManager.sendtoWait(ReturnMessage, sizeof(ReturnMessage), from))
+        Serial.println("sendtoWait failed");
+
+    }
+
+  }
+
+}

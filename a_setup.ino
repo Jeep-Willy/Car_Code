156cea7f4922e74a1e83d2dd06f170e260e18644
wabowen89 <wabowen89@gmail.com>
2023-10-24 14:49:38 -0700

    setup servo pin attachment

--- a_setup.ino
+++ a_setup.ino
@@ -10,6 +10,9 @@ void setup()
   pinMode(in2, OUTPUT);
   digitalWrite(in1, LOW);
 	digitalWrite(in2, LOW);
+
+  // Set servo pin
+  myServo.attach(servPin);
   
   // Initialize RadioManager with defaults - 2.402 GHz (channel 2), 2Mbps, 0dBm
   if (!RadioManager.init())
